2cc706904e72717e329b319280409f28b727dffb
Nico301188 <nico301188@gmail.com>
2024-05-29 15:39:36 -0300

    commit

---
+++ src/routes/checkout.routes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+import { createCheckoutPreference } from '../controllers/CheckoutController.js';
+
+const route = express.Router();
+
+route
+    .post("/", createCheckoutPreference)
+
+export default route;
